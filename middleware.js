21bbc850e233368bfeb23e3bab24e94137ca7289
SebastinST <mingchensebastin.goh@stengg.com>
2023-12-21 10:00:50 +0800

    Assignment 3 remove unnecessary middleware function ValidateUser

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-const bcrypt = require("bcryptjs");
 const connection = require("./config/database");
 
 // AS002: Check if endpoint contains special characters
@@ -24,25 +23,4 @@ exports.Checkgroup = async (userid, groupname) => {
   } else {
       return false;
   } 
-}
-
-exports.validateUser = async (username, password, connection) => {
-  
-  // Get user details from DB
-  const result = await connection.promise().query("SELECT * FROM user WHERE username = ?", [username])
-  const user = result[0][0];
-
-  // Return false if no user found
-  if (!user) {
-    return false;
-  }
-
-  // Return false if hashed provided password and DB password don't match
-  const isPasswordMatched = await bcrypt.compare(password, user.password)
-  if (!isPasswordMatched) {
-    return false;
-  }
-
-  // Return user details
-  return user;
 }
